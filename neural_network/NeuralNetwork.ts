48a4cb385e449208798f2ca7f9b56492251b2f50
NotAsExpected <ksztnnnn@gmail.com>
2024-03-16 15:30:01 +0100

    Neural Network Typescript

--- neural_network/NeuralNetwork.ts
+++ neural_network/NeuralNetwork.ts
@@ -11,15 +11,13 @@ class Neuron
  ScalingFunction: ScalingFunction;
 
  Iteration: number = 0;
- SubIteration: number = 0;
- MaxSubIterations: number = 0;
+ SubIteration: number = 0; //Used only for building of the CalculatedConnectionsPlay
+ MaxSubIterations: number = 0; //Used only for building of the CalculatedConnectionsPlay
 
- constructor(LastNominal: number, MaxSubIterations,ScalingFunction: ScalingFunction)
+ Index: number = 0;
+
+ constructor(LastNominal: number, MaxSubIterations,ScalingFunction: ScalingFunction = ScalingFunctions[0])
  {
-    if (ScalingFunction == null)
-    {
-     ScalingFunction = ScalingFunctions[0];
-    }
     this.ScalingFunction = ScalingFunction;
     this.Last_nominal = LastNominal;
     this.MaxSubIterations = MaxSubIterations;
@@ -33,7 +31,7 @@ class Connection
  ConnectionWeight : number;
  ScalingFunction : ScalingFunction;
 
- constructor(NeuronId1: number,NeuronId2: number,ConnectionWeight : number = 1,ScalingFunction : ScalingFunction)
+ constructor(NeuronId1: number,NeuronId2: number,ConnectionWeight : number = 1,ScalingFunction : ScalingFunction = ScalingFunctions[0])
  {
   if (ScalingFunction == null)
   {
@@ -52,66 +50,172 @@ class Network
 
  Neurons : Neuron[] = [];
 
- InputNeurons : Neuron[] = [];
+ InputNeurons : Neuron[] = []; //Rule for updating CurrentIteration within input neurons is a bit loose. (the update might happen depending on how the neural network is built, but this feature nontheless was in intentions and also came in handy with setting values for the input neurons)
  InputNeuronsCallbackFunc : CallbackFunc[];
 
  OutputNeurons : Neuron[] = [];
  OutputNeuronsIsActivated : ActivationFunc[]; //this feature allows for the AI to perform multiple outputs at the same time
 
- Connections : Connection[][]; //[Neuron1Id][num] :  Connection
- ConnectionsBack : Connection[][]; //[Neuron2Id][num] : Connection
+ Connections : (Connection | null)[][]; //[Neuron1Id][num] :  Connection
+ ConnectionsBack : (Connection | null)[][]; //[Neuron2Id][num] : Connection
 
  CalculatedConnectionsPlay : Connection[]; 
 
  processConnection(Connection: Connection)
  {
+  let NeuronA = this.Neurons[Connection.NeuronId1];
+  let NeuronB = this.Neurons[Connection.NeuronId2];
 
+  let Val1 = Connection.ScalingFunction((NeuronA.ScalingFunction(NeuronA.Function_value) + NeuronA.Last_nominal) * Connection.ConnectionWeight);
+
+  if (NeuronB.Iteration != this.CurrentIteration)
+  {
+   NeuronB.Iteration = this.CurrentIteration;
+   NeuronB.Function_value = Val1;
+  }
+
+  NeuronB.Function_value += Val1;
  }
 
  calculateConnectionsPlay() //This allows to put the neural network into 1 dimensional array, which can easily be processed.
  {
+  this.CurrentIteration += 1;
+  let CalculatedConnectionsPlayNew = [];
   
+  let NextIterable = []; //replaces Iterable after the end of for loop
+
+  let Iteratable = this.InputNeurons;
+
+  while (Iteratable.length > 0)
+  {
+   for (let i = 0;i < Iteratable.length;i++)
+   {
+    let Neuron : Neuron = Iteratable[i];
+    
+    if (Neuron.Iteration != this.CurrentIteration)
+    {
+      Neuron.Iteration = this.CurrentIteration;
+      Neuron.SubIteration = 0;
+    }
+
+
+
+
+   } 
+   Iteratable = NextIterable;
+  }
+
+  this.CalculatedConnectionsPlay = CalculatedConnectionsPlayNew;
  }
 
- destroy(NeurondId : number)
+ destroy(NeuronId : number)
  {
-  
+  let Connections = this.Connections[NeuronId];
+
+  if (Connections)
+  {
+   Connections.forEach(Connection => {
+    if (!Connection)
+    {
+     return; 
+    }
+    this.disconnect(Connection);
+   }) 
+  }
+
+  let Neuron = this.Neurons[NeuronId];
+  let InnerIndex = Neuron.Index;
+
+  let Ind = this.InputNeurons.findIndex((Neuron2) => {
+   return Neuron === Neuron2;
+  });
+
+  if (Ind > 0)
+  {
+    this.InputNeurons[Ind] = this.InputNeurons[this.InputNeurons.length-1];
+    this.InputNeurons.pop();
+  }
+  else if (Ind == 0)
+  {
+    this.InputNeurons.pop();
+  }
+
+
+   Ind = this.OutputNeurons.findIndex((Neuron2) => {
+    return Neuron === Neuron2;
+   });
+ 
+   if (Ind > 0)
+   {
+     this.OutputNeurons[Ind] = this.OutputNeurons[this.OutputNeurons.length-1];
+     this.OutputNeurons.pop();
+   }
+   else if (Ind == 0)
+   {
+     this.OutputNeurons.pop();
+   }
+ 
+   for (let i = InnerIndex;i < this.Neurons.length-1;i++)
+   {
+    this.Neurons[i] = this.Neurons[i+1];
+    this.Neurons[i].Index -= 1;
+   }
+
+   this.Neurons.pop();
  }
 
- add(Type : string,CallbackFunc: CallbackFunc | null = null,NeuronScalingFunction : ScalingFunction | null)
+ add(Type : string,CallbackFunc: any = null,NeuronScalingFunction : ScalingFunction)
  {
-  
+  let Neuron_ = new Neuron(0,0,NeuronScalingFunction);
+
+  if (Type == "Input")
+  {
+   this.InputNeurons.push(Neuron_);
+   this.InputNeuronsCallbackFunc = CallbackFunc;
+  }
+  else if (Type == "Output")
+  {
+   this.OutputNeurons.push(Neuron_);
+   this.OutputNeuronsIsActivated = CallbackFunc;
+  }
+
+  this.Neurons.push(Neuron_);
+
+  Neuron_.Index = this.Neurons.length-1;
+
+  return Neuron_;
  }
 
- disconnect(NeurondId1: number,NeurondId2: number) 
+ disconnect(Connection: Connection) 
  {
-  
+  this.Connections[Connection.NeuronId1][Connection.NeuronId2] = null;
+  this.ConnectionsBack[Connection.NeuronId2][Connection.NeuronId1] = null;
  }
 
- connect(NeurondId1: number,NeurondId2: number,Weight: number,ScalingFunction: ScalingFunction) : Connection
+ connect(NeuronId1: number,NeuronId2: number,Weight: number,ScalingFunction: ScalingFunction) : Connection
  {
-  let Connections = this.Connections[NeurondId1];
-  let Connection_ = new Connection(NeurondId1,NeurondId2,Weight,ScalingFunction);
+  let Connections = this.Connections[NeuronId1];
+  let Connection_ = new Connection(NeuronId1,NeuronId2,Weight,ScalingFunction);
   
   if (!Connections)
   {
-    this.Connections[NeurondId1] = [];
-    this.Connections[NeurondId1][NeurondId2] = Connection_;
+    this.Connections[NeuronId1] = [];
+    this.Connections[NeuronId1][NeuronId2] = Connection_;
 
-    this.ConnectionsBack[NeurondId2] = this.ConnectionsBack[NeurondId2] || [];
-    this.ConnectionsBack[NeurondId2][NeurondId1] = Connection_;
+    this.ConnectionsBack[NeuronId2] = this.ConnectionsBack[NeuronId2] || [];
+    this.ConnectionsBack[NeuronId2][NeuronId1] = Connection_;
 
     return Connection_;
   }
 
-  let Connection_2 = Connections[NeurondId2];
+  let Connection_2 = Connections[NeuronId2];
  
   if (!Connection_2)
   {
-    this.Connections[NeurondId1][NeurondId2] = Connection_;
+    this.Connections[NeuronId1][NeuronId2] = Connection_;
 
-    this.ConnectionsBack[NeurondId2] = this.ConnectionsBack[NeurondId2] || [];
-    this.ConnectionsBack[NeurondId2][NeurondId1] = Connection_;
+    this.ConnectionsBack[NeuronId2] = this.ConnectionsBack[NeuronId2] || [];
+    this.ConnectionsBack[NeuronId2][NeuronId1] = Connection_;
 
     return Connection_;
   }
@@ -125,13 +229,15 @@ class Network
         return false;
     }
 
+    this.CurrentIteration += 1;
+
     for (let i = 0; i < this.CalculatedConnectionsPlay.length; i++) {
         let Connection : Connection = this.CalculatedConnectionsPlay[i];
         this.processConnection(Connection);
     }
 
     return true;
-}
+ }
 
  iterate() 
  {
